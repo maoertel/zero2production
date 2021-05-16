fef116a304b7e477cf0355f5e47ec09222b99410
mortar <53ff0bae@opayq.com>
2021-05-16 09:33:04 +0200

    add subscriptions route and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,27 @@
+use std::net::TcpListener;
+
 use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 use actix_web::dev::Server;
-use std::net::TcpListener;
 
 async fn healthz() -> impl Responder {
   HttpResponse::Ok()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+  HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+  email: String,
+  name: String,
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
   let server = HttpServer::new(|| {
     App::new()
       .route("/healthz", web::get().to(healthz))
+      .route("/subscriptions", web::post().to(subscribe))
   })
     .listen(listener)?
     .run();
--- tests/healthz.rs
+++ tests/healthz.rs
@@ -28,3 +28,52 @@ fn spawn_app() -> String {
   let _ = tokio::spawn(server);
   format!("http://127.0.0.1:{}", port)
 }
+
+#[actix_rt::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+  // Given
+  let app_address = spawn_app();
+  let client = reqwest::Client::new();
+  let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+  // When
+  let response = client
+    .post(&format!("{}/subscriptions", &app_address))
+    .header("Content-Type", "application/x-www-form-urlencoded")
+    .body(body)
+    .send()
+    .await
+    .expect("Failed to execute request.");
+
+  // Then
+  assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+  // Given
+  let app_address = spawn_app();
+  let client = reqwest::Client::new();
+  let test_cases = vec![
+    ("name=le%20guin", "missing the email"),
+    ("email=ursula_le_guin%40gmail.com", "missing the name"),
+    ("", "missing both name and email")
+  ];
+  for (invalid_body, error_message) in test_cases {
+    // When
+    let response = client
+      .post(&format!("{}/subscriptions", &app_address))
+      .header("Content-Type", "application/x-www-form-urlencoded")
+      .body(invalid_body)
+      .send()
+      .await
+      .expect("Failed to execute request.");
+    // Then
+    assert_eq!(
+      400,
+      response.status().as_u16(),
+      "The API did not fail with 400 Bad Request when the payload was {}.",
+      error_message
+    );
+  }
+}
