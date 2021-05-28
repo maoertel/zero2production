29003a22e579d5eb6b1f047cc216fa3699a480d1
mortar <53ff0bae@opayq.com>
2021-05-28 08:55:13 +0200

    factor out telemetry

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![allow(clippy::toplevel_ref_arg)]
+
 pub mod config;
 pub mod routes;
 pub mod startup;
+pub mod telemetry;
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{EnvFilter, layer::SubscriberExt, Registry};
 
 use zero2prod::config::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -22,19 +19,8 @@ async fn main() -> std::io::Result<()> {
     .await
     .expect("Failed to connect ot Postgres.");
 
-  LogTracer::init().expect("Failed to set logger");
-  let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-  let formatting_layer = BunyanFormattingLayer::new(
-    "zero2prod".into(),
-    std::io::stdout,
-  );
-
-  let subscriber = Registry::default()
-    .with(env_filter)
-    .with(JsonStorageLayer)
-    .with(formatting_layer);
-
-  set_global_default(subscriber).expect("Failed to set subscriber.");
+  let subscriber = get_subscriber("zero2prod".into(), "info".into());
+  init_subscriber(subscriber);
 
   run(listener, connection_pool)?.await
 }
